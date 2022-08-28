091c45c1d9a857fcbaf85c05d851b71f9a1010a0
Kephas <odurof99@gmail.com>
2022-08-28 05:22:20 +0200

    excercises solved

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  test: {
-    include: ["src/**/*.ts"],
-    setupFiles: ["scripts/setup.ts"],
-    passWithNoTests: true,
-  },
+	test: {
+		include: ['src/**/*.ts'],
+		setupFiles: ['scripts/setup.ts'],
+		passWithNoTests: true,
+	},
 });
